b54755519ed36147d1105810a5e2fe89e6db3227
RomanYuts <54900116+RomanYuts@users.noreply.github.com>
2021-05-28 12:22:15 +0300

    Lab_6 commit

---
+++ Lab_6/mongoose.js
@@ -0,0 +1,5 @@
+var mongoose=require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://Admin:admin12345@cluster0-shard-00-00.673lq.mongodb.net:27017,cluster0-shard-00-01.673lq.mongodb.net:27017,cluster0-shard-00-02.673lq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-o63m6a-shard-0&authSource=admin&retryWrites=true&w=majority');
+console.log("mongodb connect...")
+module.exports=mongoose;
